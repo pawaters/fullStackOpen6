b25ea85a1e3da0ffbbf4dceba5633ec011b50de8
pawaters <86101754+pawaters@users.noreply.github.com>
2022-11-14 10:58:39 +0200

    6.1 WIP

---
+++ my-app/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+
+const noteReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.data)
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.data.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = { 
+        ...noteToChange, 
+        important: !noteToChange.important 
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote 
+      )
+     }
+    default:
+      return state
+  }
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
+  return(
+    <div>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => root.render(<App />)
+
+renderApp()
+store.subscribe(renderApp)
