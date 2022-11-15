a9bb69563fff28f48ab6d853d037cbdf6012f44e
pawaters <86101754+pawaters@users.noreply.github.com>
2022-11-15 14:34:43 +0200

    notificationReducer

---
+++ anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,31 @@
+// define reducer cases
+const notificationReducer = (state = null, action) => {
+    switch (action.type) {
+        case 'NEW_NOTIFICATION':
+            return action.notification
+        case 'HIDE_NOTIFICATION':
+            return action.notification
+        default: 
+            return state
+    }
+}
+
+// define time of notif
+
+export const setNotification = (notification, displayTime) => {
+    return async dispatch => {
+        dispatch({
+            type: 'NEW_NOTIFICATION',
+            notification,
+        })
+
+        setTimeout(() => {
+            dispatch({
+                type: 'HIDE_NOTIFICATION',
+                notification: null
+            })
+        }, displayTime * 1000)
+    }
+}
+
+export default notificationReducer
