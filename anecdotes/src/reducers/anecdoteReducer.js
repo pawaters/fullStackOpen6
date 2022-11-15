d80833bbf1e27764cdc995d879dfd29bec9d086e
pawaters <86101754+pawaters@users.noreply.github.com>
2022-11-15 15:20:50 +0200

    anecdoteReducer done

--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,6 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import anecdoteService from '../services/anecdotes'
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+//initialisation has all been sent to db.
 
 const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
@@ -44,8 +27,36 @@ const anecdoteReducer = (state = [], action) => {
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
-    const anecdotes = awaut anecdotes
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
   }
 }
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const  newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote,
+    })
+  }
+}
+
+export const vote = anecdote => {
+  return async dispatch => {
+    const updatedAnecdote = await anecdoteService.update({
+      ...anecdote,
+      votes: anecdote.votes + 1
+    })
+    dispatch({
+      type: 'VOTE',
+      data: updatedAnecdote
+    })
+  }
+}
+
 
 export default anecdoteReducer
